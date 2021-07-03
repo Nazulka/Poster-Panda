db6f977b79d274e25e068a87d5d495ff0a9b73be
Nazulka <nazeekadiu@yahoo.co.uk>
2021-07-03 10:59:01 +0000

    Make initial migrations

--- reviews/models.py
+++ reviews/models.py
@@ -1,24 +1,25 @@
 from django.db import models
 
-from profiles.models import Profile
+from profiles.models import UserProfile
 from products.models import Product
 
 # Create your models here.
 
 class ProductReview(models.Model):
 
-    RATING_CHOICES = (
+    RATING_CHOICES = [
     (1, 'Poor'),
     (2, 'Average'),
     (3, 'Good'),
     (4, 'Very Good'),
     (5, 'Excellent')
-    )
+    ]
 
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     headline = models.CharField(max_length=50, null=False, blank=False)
     comments = models.CharField(max_length=1200, null=False, blank=False)
+    rating = models.IntegerField(choices=RATING_CHOICES)
     date_posted = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
