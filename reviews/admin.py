6f01cb68075dce98e866424f88c67ba0b6a69c68
Nazulka <nazeekadiu@yahoo.co.uk>
2021-07-03 12:18:07 +0000

    Add product review admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import ProductReview
 
 # Register your models here.
+
+class ProductReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'product',
+        'headline',
+        'comments',
+        'rating',
+        'date_posted'
+    )
+
+
+admin.site.register(ProductReview, ProductReviewAdmin)
