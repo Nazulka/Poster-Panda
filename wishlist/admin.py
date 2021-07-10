08fa27a6f120fe36642fd113bbf8853858687dc4
Nazulka <nazeekadiu@yahoo.co.uk>
2021-07-10 19:52:49 +0000

    Add wishlist urls and admin

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
-# from wishlist.models import Wishlist, WishlistItem
+from wishlist.models import Wishlist, WishlistItem
 
-# # class WishlistAdmin(admin.ModelAdmin):
-# #     list_display = (
-# #             'wishlist',
-# #             'product',
-# #         )
 
-# admin.site.register(Wishlist, WishlistItem)
+admin.site.register(Wishlist)
+admin.site.register(WishlistItem)
