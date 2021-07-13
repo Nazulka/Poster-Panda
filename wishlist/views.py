da506c523bbc3a388144e004ef53974cc86ad223
Nazulka <nazeekadiu@yahoo.co.uk>
2021-07-13 17:09:28 +0000

    Debug wishlist views and template

--- wishlist/views.py
+++ wishlist/views.py
@@ -3,29 +3,35 @@ from django.contrib import messages
 
 from products.models import Product
 from profiles.models import UserProfile
+from wishlist.models import Wishlist, WishlistItem
+
 
 
 def add_to_wishlist(request, product_id):
     """ A view to add products to the wishlist and remove from it"""
+
     user = UserProfile.objects.get(user=request.user)
     product = get_object_or_404(Product, pk=product_id)
-    wishlists = user.wishlist.all()
+    wishlist = Wishlist.objects.all()
+    # wishlists = user.wishlist.all()
   
-    wishlist = Wishlist.objects.get.all()
-
     if request.method == 'POST':
 
-        if wishlist.products.filter(pk=request.user.id).exists():
-            wishlist.products.remove(request.user)
+        if wishlist.product.filter(pk=request.user.id).exists():
+            wishlist.product.remove(request.user)
+            
             messages.success(request, 'Successfully removed the item from \
                             the wishlist!')
         else:
             wishlist.product.add(request.user)
-            messages.success(request, 'Successfully added the item from \
-                            the wishlist!')
-
+            messages.success(request, 'Successfully added the item to the \
+                             wishlist! ')
+                             
+    print(f"WISHLIST: {wishlist}")
+    template = 'wishlist/wishlist.html'
     context = {
+        'product': product,
         'wishlist': wishlist,
     }
 
-    return render(request, 'wishlist/wishlist.html', context)
+    return render(request, template, context)
