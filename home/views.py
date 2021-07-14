a5574d8b4fec605e715b780cf36a79baee8e28bb
Nazulka <nazeekadiu@yahoo.co.uk>
2021-07-14 13:36:31 +0000

    Add Future Features section

--- home/views.py
+++ home/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 
 
 def index(request):
     """ A view to return the index page """
+    products = Product.objects.all()
 
-    return render(request, 'home/index.html')
+    context = {
+        'products': products,
+    }
+    template = 'home/index.html'
+    
+    return render(request, template, context)
