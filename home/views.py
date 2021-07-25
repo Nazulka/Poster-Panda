17ea307303bea7852cd467b5ae82780e6ef858d1
Nazulka <nazeekadiu@yahoo.co.uk>
2021-07-25 23:55:03 +0000

    Add brand-logo image for mobile nav

--- home/views.py
+++ home/views.py
@@ -1,6 +1,10 @@
 import random
 from django.shortcuts import render
 from products.models import Product
+from profiles.models import UserProfile
+
+from wishlist.models import Wishlist, WishlistItem
+
 
 
 # Create your views here.
@@ -11,11 +15,16 @@ def index(request):
 
     # products = Product.objects.all()
     pool = list(Product.objects.all())
+    user = UserProfile.objects.get(user=request.user)
+    wishlist = Wishlist.objects.get(user=user)
+    wishlist_products = WishlistItem.objects.filter(wishlist=wishlist)
     random.shuffle(pool)
 
     context = {
         # 'products': products,
         'pool': pool,
+        'wishlist_products': wishlist_products,
+
     }
     template = 'home/index.html'
 
