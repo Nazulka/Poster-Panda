e6de4f6cdbc28abe3fa3b428c829202ffb9141cd
Nazulka <nazeekadiu@yahoo.co.uk>
2021-08-05 10:37:28 +0000

    Code formatting + validating

--- home/views.py
+++ home/views.py
@@ -1,8 +1,6 @@
 import random
 from django.shortcuts import render
 from products.models import Product
-# from profiles.models import UserProfile
-# from wishlist.models import Wishlist, WishlistItem
 
 
 # Create your views here.
@@ -10,16 +8,11 @@ from products.models import Product
 def index(request):
     """ A view to return the index page """
 
-    # user = UserProfile.objects.get(user=request.user)
-    # wishlist = Wishlist.objects.get(user=user)
-    # wishlist_products = WishlistItem.objects.filter(wishlist=wishlist)
-
     pool = list(Product.objects.all())
     random.shuffle(pool)
 
     context = {
         'pool': pool,
-        # 'wishlist_products': wishlist_products,
 
     }
     template = 'home/index.html'
