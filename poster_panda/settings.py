4c01f21fbe4774be4c1f65d6eb1771c21fc757a9
Nazulka <nazeekadiu@yahoo.co.uk>
2021-08-09 07:37:15 +0000

    Turn DEBUG to False

--- poster_panda/settings.py
+++ poster_panda/settings.py
@@ -25,10 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if 'DEVELOPMENT' in os.environ:
-    DEBUG = True
-else:
-    DEBUG = False
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['poster-panda.herokuapp.com', 'localhost']
